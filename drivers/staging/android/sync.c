8738841dde1aa54c2745d7e596d06257fa0469ba
Dmitry Torokhov <dtor@chromium.org>
2015-12-14 17:34:08 -0800

    android: unconditionally remove callbacks in sync_fence_free()
    [ Upstream commit 699f685569434510d944e419f4048c4e3ba8d631 ]
    
    Using fence->status to determine whether or not there are callbacks
    remaining on the sync_fence is racy since fence->status may have been
    decremented to 0 on another CPU before fence_check_cb_func() has
    completed.  By unconditionally calling fence_remove_callback() for each
    fence in the sync_fence, we guarantee that each callback has either
    completed (since fence_remove_callback() grabs the fence lock) or been
    removed.
    
    Signed-off-by: Andrew Bresticker <abrestic@chromium.org>
    Signed-off-by: Dmitry Torokhov <dtor@chromium.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- drivers/staging/android/sync.c
+++ drivers/staging/android/sync.c
@@ -639,8 +639,13 @@ static void sync_fence_free(struct kref *kref)
 {
 	struct sync_fence *fence = container_of(kref, struct sync_fence, kref);
 
-	sync_fence_free_pts(fence);
+	int i;
 
+	for (i = 0; i < fence->num_fences; ++i) {
+		fence_remove_callback(fence->cbs[i].sync_pt, &fence->cbs[i].cb);
+		fence_put(fence->cbs[i].sync_pt);
+	}
+	sync_fence_free_pts(fence);
 	kfree(fence);
 }
 
