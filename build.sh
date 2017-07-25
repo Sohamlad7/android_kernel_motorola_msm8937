6ab6f85f420d23fe602be76d38ad25b410297958
Sohamlad7 <sohamlad7@gmail.com>
2017-07-25 12:07:21 +0530

    build: Update framework for cedric
    Change-Id: Ic606ffd4005f16e44061c8c197541d4d3292ac10

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 ### Prema Chand Alugu (premaca@gmail.com)
 ### Shivam Desai (shivamdesaixda@gmail.com)
 ### Soham Lad (sohamlad7@gmail.com)
-### A custom build script to build zImage & DTB(Anykernel2 method)
+### A custom build script to build zImage,DTB & Wlan module(Anykernel2 method)
 
 set -e
 
@@ -36,9 +36,10 @@ make -j$JOBS
 echo "**** Generating DT.IMG ****"
 $DTBTOOL/dtbToolCM -2 -o $KERNEL_DIR/arch/arm/boot/dtb -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/qcom/
 
-echo "**** Verify zImage & dtb ****"
+echo "**** Verify zImage,dtb & wlan****"
 ls $KERNEL_DIR/arch/arm/boot/zImage
 ls $KERNEL_DIR/arch/arm/boot/dtb
+ls $KERNEL_DIR/drivers/staging/prima/wlan.ko
 
 #Anykernel 2 time!!
 echo "**** Verifying Anyernel2 Directory ****"
@@ -46,18 +47,24 @@ ls $ANY_KERNEL2_DIR
 echo "**** Removing leftovers ****"
 rm -rf $ANY_KERNEL2_DIR/dtb
 rm -rf $ANY_KERNEL2_DIR/zImage
+rm -rf $ANY_KERNEL2_DIR/modules/wlan.ko
 rm -rf $ANY_KERNEL2_DIR/$FINAL_KERNEL_ZIP
 
 echo "**** Copying zImage ****"
-cp $KERNEL_DIR/arch/arm/boot/zImage $ANY_KERNEL2_DIR/
+cp -rf $KERNEL_DIR/arch/arm/boot/zImage $ANY_KERNEL2_DIR/
 echo "**** Copying dtb ****"
-cp $KERNEL_DIR/arch/arm/boot/dtb $ANY_KERNEL2_DIR/
+cp -rf $KERNEL_DIR/arch/arm/boot/dtb $ANY_KERNEL2_DIR/
+echo "**** Copying modules ****"
+cd $ANY_KERNEL2_DIR/
+mkdir modules
+cd ../
+cp -rf $KERNEL_DIR/drivers/staging/prima/wlan.ko $ANY_KERNEL2_DIR/modules/
 
 echo "**** Time to zip up! ****"
 cd $ANY_KERNEL2_DIR/
 zip -r9 $FINAL_KERNEL_ZIP * -x README $FINAL_KERNEL_ZIP
 rm -rf $HOME/work/$FINAL_KERNEL_ZIP
-cp $HOME/work/android_kernel_motorola_msm8937/AnyKernel2/$FINAL_KERNEL_ZIP $HOME/work/$FINAL_KERNEL_ZIP
+cp -rf $HOME/work/android_kernel_motorola_msm8937/AnyKernel2/$FINAL_KERNEL_ZIP $HOME/work/$FINAL_KERNEL_ZIP
 
 echo "**** Good Bye!! ****"
 cd $KERNEL_DIR
@@ -65,3 +72,4 @@ rm -rf arch/arm/boot/dtb
 rm -rf $ANY_KERNEL2_DIR/$FINAL_KERNEL_ZIP
 rm -rf AnyKernel2/zImage
 rm -rf AnyKernel2/dtb
+rm -rf AnyKernel2/modules
