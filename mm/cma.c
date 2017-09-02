36e749def802db1360bf6ac0fde161c2f1e4693d
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2017-09-02 07:06:04 +0200

    Linux 3.18.69

--- mm/cma.c
+++ mm/cma.c
@@ -77,10 +77,6 @@ static unsigned long cma_bitmap_aligned_offset(const struct cma *cma,
 		- cma->base_pfn) >> cma->order_per_bit;
 }
 
-static unsigned long cma_bitmap_maxno(struct cma *cma)
-{
-	return cma->count >> cma->order_per_bit;
-}
 
 static unsigned long cma_bitmap_pages_to_bits(const struct cma *cma,
 					      unsigned long pages)
